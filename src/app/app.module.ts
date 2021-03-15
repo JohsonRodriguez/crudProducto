54e7b219ad044df57b0a95c4bed9ac9fada90748
Johson Rodriguez <sukerjc@gmail.com>
2021-03-14 23:55:21 -0500

    ad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaProductoComponent } from './producto/lista-producto/lista-producto.component';
+import { DetalleProductoComponent } from './producto/detalle-producto/detalle-producto.component';
+import { NuevoProductoComponent } from './producto/nuevo-producto/nuevo-producto.component';
+import { EditarProductoComponent } from './producto/editar-producto/editar-producto.component';
+
+// ----External---
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import {HttpClientModule} from '@angular/common/http'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductoComponent,
+    DetalleProductoComponent,
+    NuevoProductoComponent,
+    EditarProductoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    ToastrModule.forRoot()
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
